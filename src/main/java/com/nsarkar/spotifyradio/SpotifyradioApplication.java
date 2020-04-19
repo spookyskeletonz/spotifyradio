6b16ae0bc8b1146bedfe537307b159e1164e78dc
Neil.Sarkar <ansarkar@outlook.com>
2020-04-19 15:35:47 +1000

    init commit

---
+++ src/main/java/com/nsarkar/spotifyradio/SpotifyradioApplication.java
@@ -0,0 +1,13 @@
+package com.nsarkar.spotifyradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyradioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyradioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsarkar/spotifyradio/SpotifyradioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsarkar.spotifyradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyradioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
