a889e3c43c291b8a50ffaed837cdd7bdcf687fc1
Neil.Sarkar <ansarkar@outlook.com>
2020-04-20 23:30:47 +1000

    add logging also fix up registry

--- src/main/java/com/nsarkar/spotifyradio/config/WebSocketConfig.java
+++ src/main/java/com/nsarkar/spotifyradio/config/WebSocketConfig.java
@@ -11,14 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws");
     registry.addEndpoint("/ws").withSockJS();
   }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.setApplicationDestinationPrefixes("/app");
-    registry.enableSimpleBroker("channel");
+    registry.enableSimpleBroker("/channel");
   }
 
 }
--- src/main/java/com/nsarkar/spotifyradio/controller/ChatController.java
+++ src/main/java/com/nsarkar/spotifyradio/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.nsarkar.spotifyradio.controller;
 
 import com.nsarkar.spotifyradio.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,13 +12,19 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+  Logger logger = LoggerFactory.getLogger(ChatController.class);
 
   @Autowired
   SimpMessagingTemplate messagingTemplate;
 
-  @MessageMapping("chat/{channelId}/sendMessage")
+  @MessageMapping("/chat/{channelId}/sendMessage")
   public void sendMessage(@DestinationVariable String channelId, @Payload ChatMessage chatMessage) {
     // send the incoming message to all clients connected to this channel
+    if (chatMessage == null) {
+      logger.warn("Received empty message for channel {}", channelId);
+      return;
+    }
+    logger.info("Received message for channel {}: {}", channelId, chatMessage.getMessage());
     messagingTemplate.convertAndSend(String.format("/channel/%s", channelId), chatMessage);
   }
 }
